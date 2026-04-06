61fbf46bb9dd28b9488724fb58c9e55744251392
zacharyb2028 <zachary.belitz2028@gbwl.org>
2026-04-06 12:52:46 +0000

    stared js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// SINGLE LINE COMMENT
+/* MULTI-LINE COMMENT */
+
+/* The JS code below is an example STATEMENT (instruction/command) every statement ends in a SEMICOLON */
 console.log("hello world");
+
+
+
