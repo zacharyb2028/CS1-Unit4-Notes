ca5311a332079fe63aa95cb877f0319bae972c0b
zacharyb2028 <zachary.belitz2028@gbwl.org>
2026-04-13 12:46:32 +0000

    variables

--- script.js
+++ script.js
@@ -2,7 +2,27 @@
 /* MULTI-LINE COMMENT */
 
 /* The JS code below is an example STATEMENT (instruction/command) every statement ends in a SEMICOLON */
-console.log("hello world");
+console.log("welcome to JS");
 
+// VARIABLES are named storage containers for data
 
+// DECLARE a new variable (make space)
+let username;
 
+// ASSIGN a value to a variable
+username = "zachary.belitz";
+
+// see the avlue stored in a variable
+console.log(username);
+
+// DECLARE + ASSIGN in one line
+let pokemon = "pikachu";
+let luckyNum = 27; // no quotes around numbers!
+
+// look at our new variables
+console.log(pokemon);
+console.log(luckyNum);
+
+// Change the value stored in a var
+luckyNum = 15;
+console.log(luckyNum);
